540f9ad9fdaf3a70dd8fb49f0614372041161460
Pr0phet <pr0phet@abv.bg>
2018-11-08 15:26:27 +0200

    ver 0.1

--- File_managment_main.py
+++ File_managment_main.py
@@ -1,4 +1,5 @@
 import os.path
+default_path = os.getcwd()
 def spaces(number_of, symbol):
     for i in range(number_of):
         print(symbol, end='')
@@ -15,17 +16,24 @@ def menu():
     print()
     print('Current dirrectory is %s' % os.getcwd(), end='')
     print()
-def create_dir(path, name):
+def create_dir(name):
     try:
         os.mkdir(name)
     except FileExistsError:
         print('File already exist')
-def delete_dir(path, name):
-        usr_rm_dir = str(input('Name of folder to delete: '))
-        if(usr_rm_dir == ''):
-            os.rmdir('Empty')
-        else:
-            os.rmdir(usr_rm_dir)
+def delete_dir(name):
+    if(name == ''):
+        os.rmdir('Empty')
+    else:
+        os.rmdir(name)
+def create_file(name):
+    f = open(name, 'a')
+    f.close() 
+def delete_file(name):
+    if os.path.exists(name):
+        os.remove(name)
+    else:
+        print("The file does not exist")
 menu()
 try:
     usr_choice = int(input('Choose one: '))
@@ -52,23 +60,82 @@ if (usr_choice == 1):
         if(usr_dir_name == ''):
             usr_dir_name = 'Empty'
         
-        create_dir(usr_path, usr_dir_name)
+        create_dir(usr_dir_name)
 
     except(FileExistsError, FileNotFoundError, OSError) as e:
+        os.chdir(default_path)
         f = open('ErrorLog.txt', 'a')
         f.write(str(e))
         f.write('\n')
         f.close()
         print('Error!')
 elif (usr_choice == 2):
-        create_dir(usr_path, usr_dir_name)
+    try:
+        usr_path = str(input('Enter path for directory: '))
+        if(usr_path == ''):
+            usr_path = os.getcwd() 
+        os.chdir(usr_path)
+
+        usr_dir_name = str(input('Enter name for directory: '))
+        if(usr_dir_name == ''):
+            usr_dir_name = 'Empty'
+        delete_dir(usr_dir_name)
+        
+    except(FileExistsError, FileNotFoundError, OSError) as e:
+        os.chdir(default_path)     
+        f = open('ErrorLog.txt', 'a')
+        f.write(str(e))
+        f.write('\n')
+        f.close()
+        print('Error!')                                    
 elif (usr_choice == 3):
-    #file make
-    pass
+    try:
+        usr_path = str(input('Enter path for file: '))
+        if(usr_path == ''):
+            usr_path = os.getcwd() 
+        os.chdir(usr_path)
+        
+        usr_file_name = str(input('Enter name for file: '))
+        if(usr_file_name == ''):
+            usr_file_name = 'Empty'
+        
+        create_file(usr_file_name)
+
+    except(FileExistsError, FileNotFoundError, OSError) as e:
+        os.chdir(default_path)
+        f = open('ErrorLog.txt', 'a')
+        f.write(str(e))
+        f.write('\n')
+        f.close()
+        print('Error!')
 elif (usr_choice == 4):
-    #file del
-    pass
+    try:
+        usr_path = str(input('Enter path for file: '))
+        if(usr_path == ''):
+            usr_path = os.getcwd() 
+        os.chdir(usr_path)
+        
+        usr_file_name = str(input('Enter name for file: '))
+        if(usr_file_name == ''):
+            usr_file_name = 'Empty'
+        
+        delete_file(usr_file_name)
+
+    except(FileExistsError, FileNotFoundError, OSError) as e:
+        os.chdir(default_path)
+        f = open('ErrorLog.txt', 'a')
+        f.write(str(e))
+        f.write('\n')
+        f.close()
+        print('Error!')
+
 elif (usr_choice == 5):
-    #change dir
-    pass
+    usr_path = str(input('Enter the new path: '))
+    if usr_path == '':
+        usr_path = os.getcwd()
+    os.chdir(usr_path)
+    menu()
+
+
+
 
